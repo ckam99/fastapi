99b32a841abf5a730c9a31eee7b27bbfcbd626e8
Ckam <ckam225@gmail.com>
2021-09-13 21:16:16 -0700

    Change python image to alpine

--- src/apps/base/tasks.py
+++ src/apps/base/tasks.py
@@ -9,18 +9,12 @@ from celery import shared_task
 def send_confirmation_email(self, email: str):
     try:
         code = unique_code()
-        print('code====', code)
         body = {
             'email': email,
             'code': code
         }
         send_email_async('Confirmation', body=body, to=[
             email], template_name='mail/confirm_email.html')
+        return 'Done!'
     except Exception as e:
-        print("ASS :", e)
-
-
-@shared_task(name="create_task")
-def create_task(a, b, c):
-    time.sleep(a)
-    return b + c
+        return 'Error sending confirmation email'
