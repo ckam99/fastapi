be1e29f3021f23771872774263997fae97b61296
Ckam <ckam225@gmail.com>
2022-03-19 21:31:15 +0300

    fix

--- core/helpers.py
+++ core/helpers.py
@@ -14,13 +14,13 @@ def load_models():
 
 def load_tasks():
     files = ['tasks.{}'.format(x.name.split('.')[0]) for x in os.scandir(
-        'models') if x.is_file() and x.name.endswith('.py')]
+        'tasks') if x.is_file() and x.name.endswith('.py')]
     return files
 
 
 def load_signals():
     files = ['signals.{}'.format(x.name.split('.')[0]) for x in os.scandir(
-        'models') if x.is_file() and x.name.endswith('.py')]
+        'signals') if x.is_file() and x.name.endswith('.py')]
     return files
 
 
